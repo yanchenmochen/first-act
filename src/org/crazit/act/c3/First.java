a9747f7bb064291992e906ec770980cea9c74ede
songquanheng <wannachan@outlook.com>
2019-05-31 15:48:33 +0800

    [ADD]添加依赖文件，log4j.properties, activiti.cfg.xml，以及调用activiti的程序

---
+++ src/org/crazit/act/c3/First.java
@@ -0,0 +1,40 @@
+package org.crazit.act.c3;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+
+public class First {
+	public static void main(String[] args) {
+		
+		//创建流程引擎
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+		//得到流程存储服务组件
+		RepositoryService repositoryService = engine.getRepositoryService();
+		//获取流程任务组件
+		TaskService taskService = engine.getTaskService();
+		//获取流程运行时服务组件
+		RuntimeService runtimeService = engine.getRuntimeService();
+		//部署流程文件
+		repositoryService.createDeployment().addClasspathResource("first.bpmn").deploy();
+		//启动流程
+		runtimeService.startProcessInstanceByKey("myProcess");
+		
+		Task task =  taskService.createTaskQuery().singleResult();
+		System.out.println("第一个任务完成前，当前任务名称： "+task.getName());
+		System.out.println(task.getExecutionId());
+		taskService.complete(task.getId());
+		//完成第一个任务
+		task = taskService.createTaskQuery().singleResult();
+		System.out.println("第二个任务完成前，当前任务名称: "+task.getName());
+		taskService.complete(task.getId());
+		
+		task = taskService.createTaskQuery().singleResult();
+		System.out.println("流程结束后，查找任务: "+ task);
+		//退出
+		System.exit(0);
+	}
+}
